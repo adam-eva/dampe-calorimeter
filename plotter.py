3fc73263be9176b3ce4d076e76ee9ec5c5457935
kyril <kyril.kaufmann@etu.unige.ch>
2024-05-31 00:05:06 +0200

    Clean up

--- plotter.py
+++ plotter.py
@@ -1,22 +1,27 @@
 import os
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from matplotlib import colors
 import numpy as np
 
 figpath = './figures/'
 colors  = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2',
 '#7f7f7f', '#bcbd22', '#17becf']
+fs = 15
 
 ############################################################################################	
 
-def plot_image(image, savefig=False,figname=None,image_name=None):
+def plot_image(image, loglevel=False, savefig=False,figname=None,image_name=None):
     """Plot particle image"""
-    fig, ax = plt.subplots(figsize=(9,7), ncols=1)
-    im = ax.imshow(image, cmap='hot')
-    fig.colorbar(im, ax=ax, shrink=0.7)
+    fig, ax = plt.subplots(figsize=(12,7), ncols=1)
+    if loglevel:
+        im = ax.imshow(image, cmap='Blues')
+    else:
+        im = ax.imshow(image, cmap='Blues')
+    fig.colorbar(im, ax=ax, shrink=0.8)
 
-    plt.xlabel('X, Y [BGO bars]')
-    plt.ylabel('Z [BGO layers]')
+    plt.xlabel('X, Y [BGO bars]', fontsize=fs)
+    plt.ylabel('Z [BGO layers]', fontsize=fs)
 
     if image_name:
         ax.set_title(image_name)
@@ -29,12 +34,12 @@ def plot_image(image, savefig=False,figname=None,image_name=None):
 
 ############################################################################################	
 
-def plot_model_history(models, metrics=['mean_squared_error'], loglevel = False, savefig=False, figname=None):
+def plot_model_history(models, metrics=['mean_squared_error'], loglevel = False, savefig=False, figname=None, label_str=None):
     """Plot loss of the model VS epoch"""
     if not isinstance(models, list):
         models = [models]
     
-    fig = plt.figure(figsize=(7,5)) 
+    fig = plt.figure(figsize=(12,7)) 
     
     if loglevel == True:
         plt.yscale('log')
@@ -43,12 +48,12 @@ def plot_model_history(models, metrics=['mean_squared_error'], loglevel = False,
         for loss in metrics: 
             plt.plot(
             history[loss],
-            label = loss,
+            label = loss+label_str[col_idx],
             color=colors[col_idx]
             )
             plt.plot(
             history['val_'+loss],
-            label = 'val_'+loss,
+            label = 'val_'+loss+label_str[col_idx],
             color=colors[col_idx],
             linestyle='dashed'
             )
@@ -57,9 +62,9 @@ def plot_model_history(models, metrics=['mean_squared_error'], loglevel = False,
     ax = fig.gca()
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     
-    plt.xlabel('Epoch')
-    plt.ylabel('Metrics')
-    plt.legend(loc='best')
+    plt.xlabel('Epoch',fontsize=fs)
+    plt.ylabel('Metrics',fontsize=fs)
+    plt.legend(loc='best',fontsize=fs)
     plt.grid()
     
     if savefig:
@@ -76,19 +81,21 @@ def plot_model_history(models, metrics=['mean_squared_error'], loglevel = False,
 
 def plot_split_distribution(dampe_data, train, val, test, figname=None):
     """Bar plot for split of the data distribution"""
-    plt.figure(figsize=(4,4))
+    plt.figure(figsize=(12,7))
     sample_size = dampe_data['images'].shape[0]
     try:
         plt.bar(
         ['train', 'validation', 'test'],
         [train.shape[0] / sample_size, val.shape[0] / sample_size, test.shape[0] / sample_size],
-        width=0.1
+        width=0.1,
+        color='gray'
         )
     except: 
         plt.bar(
         ['train', 'validation'],
         [xy_train.shape[0] / sample_size, xy_val.shape[0] / sample_size],
-        width=0.1
+        width=0.1,
+        color='gray'
         )
 
     plt.ylabel('Ratio')
@@ -98,6 +105,33 @@ def plot_split_distribution(dampe_data, train, val, test, figname=None):
     else:
         plt.show()
 
+def plot_energy(dampe_data, loglevel=False, figname=None):
+    """Energy distribution"""
 
+    plt.figure(figsize=(12,7))
 
+    plt.plot(
+    dampe_data['calorimeter_data'][:,0],
+    color=colors[2],
+    label='Total Energy'
+    )
+    plt.plot(
+    dampe_data['calorimeter_data'][:,1],
+    color=colors[3],
+    label='Max Energy'
+    )
+    
+    if loglevel == True:
+        plt.yscale('log')
+    plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+    plt.xlabel('image index',fontsize=fs)
+    plt.ylabel('Energy [MeV]',fontsize=fs)
+    plt.legend(loc='best',fontsize=fs)
+    plt.grid()
+
+    if figname:
+        plt.savefig(figpath+figname)
+    else:
+        plt.show()
+    
 

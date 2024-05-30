a58a5ce978d1756ba2232f6d25cefb0c1a134fbb
kyril <kyril.kaufmann@etu.unige.ch>
2024-05-30 12:59:10 +0200

    helpers and plotter functions

--- helpers.py
+++ helpers.py
@@ -1,5 +1,23 @@
+import os
+import json
 import numpy as np
 import matplotlib.pyplot as plt
+
+DATA_FOLDER= './models/' 
+
+def get_data_dict():
+    """Get the data and order in a dictionary"""
+    dampe_data = {}
+    calorimeter_images, calorimeter_data, data_target, benchmark_data = get_input_data()
+
+    dampe_data['images'] = calorimeter_images
+    dampe_data['calorimeter_data'] = calorimeter_data
+    dampe_data['data_target'] = data_target
+    dampe_data['benchmark_data'] = benchmark_data
+    
+    return dampe_data
+
+#####################################################################################
 
 def split_xy_intercept(image):
     """Split the image in two images.
@@ -22,3 +40,21 @@ def split_xy_intercept(image):
             y_projection = image[:,1::2,:]
 
     return x_projection, y_projection
+
+#####################################################################################
+
+def save_model_history(history, filename='history'):
+    """Save the fiting history to a json format"""
+    if not os.path.isdir(DATA_FOLDER):
+        os.mkdir(DATA_FOLDER)
+    with open(DATA_FOLDER+filename+'.json', 'w') as ff:
+        json.dump(history.history, ff)
+
+
+def load_model_history(filename):
+    """Load the fiting history from the json file"""
+    with open(filename,'r') as ff:
+        history = json.load(ff)
+        
+    return history
+
--- plotter.py
+++ plotter.py
@@ -1,5 +1,13 @@
+import os
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import numpy as np
+
+figpath = './figures/'
+colors  = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2',
+'#7f7f7f', '#bcbd22', '#17becf']
+
+############################################################################################	
 
 def plot_image(image, savefig=False,figname=None,image_name=None):
     """Plot particle image"""
@@ -12,3 +20,51 @@ def plot_image(image, savefig=False,figname=None,image_name=None):
         plt.savefig('./figures/dampe_'+figname+'.png')
     else:
         plt.show()
+    
+    return fig, ax
+
+############################################################################################	
+
+def plot_model_history(models, metrics=['mean_squared_error'], loglevel = False, savefig=False, figname=None):
+    """Plot loss of the model VS epoch"""
+    if not isinstance(models, list):
+        models = [models]
+    
+    fig = plt.figure(figsize=(7,5)) 
+    
+    if loglevel == True:
+        plt.yscale('log')
+    
+    for col_idx, history in enumerate(models): 
+        for loss in metrics: 
+            plt.plot(
+            history[loss],
+            label = loss,
+            color=colors[col_idx]
+            )
+            plt.plot(
+            history['val_'+loss],
+            label = 'val_'+loss,
+            color=colors[col_idx],
+            linestyle='dashed'
+            )
+    
+    # Force integer x axis  
+    ax = fig.gca()
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    
+    plt.xlabel('Epoch')
+    plt.ylabel('Metrics')
+    plt.legend(loc='best')
+    plt.grid()
+    
+    if savefig:
+        if not os.path.isdir(figpath):
+            os.mkdir(figpath)
+        if figname == None: 
+            figname = 'model_history'
+        plt.savefig(figpath+'dampe_'+figname+'.png')
+    else:
+        plt.show()
+
+    return fig
